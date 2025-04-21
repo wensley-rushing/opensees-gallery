e897ad4bfe1d173880601ce237975ac3c640d3e8
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-04-20 22:39:07 -0700

    cmp

---
+++ content/en/examples/test-drucker/drucker.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    t, *U = np.loadtxt("out/displacements1.out").T
+    n = 400
+    for u in U:
+        plt.plot(t[:n], u[:n])
+    plt.savefig("img/u.png")
---
+++ content/en/examples/test-drucker/index.md
@@ -0,0 +1,10 @@
+---
+title: Drucker Prager Triaxial Test
+downloads:
+  Python: ["drucker.py"]
+  Tcl: ["drucker.tcl"]
+weight: 200
+---
+
+![](img/u.png)
+
