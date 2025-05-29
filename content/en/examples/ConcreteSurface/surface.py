b3f024c0d3124bb6931c428b87a611d11d171378
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-05-28 23:18:52 -0700

    Update surface.py

--- content/en/examples/ConcreteSurface/surface.py
+++ content/en/examples/ConcreteSurface/surface.py
@@ -9,62 +9,59 @@ except:
 
 
 def create_model(material):
-#   ops = _ops
-#   ops.wipe()
-#   ops.model('basic', ndm=2, ndf=2)
-    ops = _ops.Model(ndm=2, ndf=2)
+    model = _ops.Model(ndm=2, ndf=2)
 
-    ops.nDMaterial(*material)
+    model.nDMaterial(*material)
 
     # the plane stress
-    ops.section('PlaneStress', 1, 1, 1.0)
+    model.section('PlaneStress', 1, 1, 1.0)
 
     # Create a triangle patch
-    ops.node(1, 0, 0)
-    ops.node(2, 1, 0)
-    ops.node(3, 0, 1)
-    ops.element('tri31', 1,   (1, 2, 3),   section=1)
+    model.node(1, 0, 0)
+    model.node(2, 1, 0)
+    model.node(3, 0, 1)
+    model.element('tri31', 1,   (1, 2, 3),   section=1)
 
     # Create fixities
-    ops.fix(1,   1, 1)
-    ops.fix(2,   0, 1)
-    ops.fix(3,   1, 0)
-    return ops
+    model.fix(1,   1, 1)
+    model.fix(2,   0, 1)
+    model.fix(3,   1, 0)
+    return model
 
-def analyze_dir(ops, dX, dY, sig0):
+def analyze_dir(model, dX, dY, sig0):
     dLambda = 0.02
     dLambdaMin = 0.00005
 
     print("Analyze direction ({:8.3g}, {:8.3g})".format(dX, dY))
 
-    ops.reset()
+    model.reset()
 
     # Define a simple ramp time series
-    ops.timeSeries('Linear', 1, '-factor', 2.0*sig0)
+    model.timeSeries('Linear', 1, '-factor', 2.0*sig0)
 
     # Define load pattern for imposed stresses in the given direction
-    ops.pattern('Plain', 1, 1)
-    ops.load(2,  dX, 0.0, pattern=1)
-    ops.load(3, 0.0,  dY, pattern=1)
+    model.pattern('Plain', 1, 1)
+    model.load(2,  dX, 0.0, pattern=1)
+    model.load(3, 0.0,  dY, pattern=1)
 
     # analyze
-    ops.constraints('Plain') #Transformation')
-    ops.numberer('Plain')
-    ops.system('FullGeneral')
+    model.constraints('Plain') #Transformation')
+    model.numberer('Plain')
+    model.system('FullGeneral')
 #   ops.test('NormDispIncr', 1.0e-8, 20, 0)
-    ops.test('NormUnbalance', 1.0e-8, 50, 0)
-    ops.algorithm('Newton')
+    model.test('NormUnbalance', 1.0e-8, 50, 0)
+    model.algorithm('Newton')
 
     Lambda = 0.0
     sX = 0.0
     sY = 0.0
     while True:
-        ops.integrator('LoadControl', dLambda)
+        model.integrator('LoadControl', dLambda)
 #       ops.test('NormUnbalance', 1.0e-10, 20, 0)
-        ops.analysis('Static')
-        ok = ops.analyze(1)
-        if ok == 0:
-            stress = ops.eleResponse(1, 'material', 1, 'stress')
+        model.analysis('Static')
+
+        if model.analyze(1) == 0:
+            stress = model.eleResponse(1, 'material', 1, 'stress')
             sX = stress[0]
             sY = stress[1]
             Lambda += dLambda
@@ -94,6 +91,7 @@ def walk_surface(material, sig0):
 
         model = create_model(material)
         sX,sY = analyze_dir(model, dX, dY, sig0)
+        # TODO: shouldnt have to scale by sqrt(3)
         SX[i] = sX/(sig0*np.sqrt(3))
         SY[i] = sY/(sig0*np.sqrt(3))
     return SX, SY
@@ -141,14 +139,11 @@ if __name__ == "__main__":
 
     # Collect arguments for defining the material
     material = [
-        'FariaPlasticDamage', 1, E, v, ft, fc, #*np.sqrt(3),
+        'FariaPlasticDamage', 1, E, v, ft, fc,
         '-beta', 0.5, '-An', 3, '-Bn', 0.75, '-Ap', 2.5
     ]
 
-#   m = plot_surface(material, sig0)
-#   if not m:
     fig, ax = plt.subplots(1,1)
-#   ax.plot(*walk_surface(["ElasticIsotropic", *material[1:4]], sig0))
     SX, SY = walk_surface(material[:6], sig0)
     ax.plot(SX, SY)
 #   ax.plot(*walk_surface(material, sig0))
@@ -159,7 +154,6 @@ if __name__ == "__main__":
     ax.grid(linestyle=':')
     ax.set(xlim=[1.1*min(*SX,*SY), 1.2*max(*SX, *SY)],
            ylim=[1.1*min(*SX,*SY), 1.2*max(*SX, *SY)])
-#   ax.set_aspect('square', 'box')
     ax.axis("square")
     plt.show()
 
